e8ad7f166e2ed2672512a7db0b3c8f025e51f17a
Florin Romulescu <romulescu.florin.dev@gmail.com>
2024-02-24 09:37:44 +0200

    Register models to the admin page

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Owner, Project, Tag
 
 # Register your models here.
+admin.site.register(Owner)
+admin.site.register(Project)
+admin.site.register(Tag)

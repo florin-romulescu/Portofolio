8eda2f687d9b8b29c633fd50c6aba742275cb8de
Florin Romulescu <romulescu.florin.dev@gmail.com>
2024-02-24 09:38:33 +0200

    Index Page Class

--- web/views.py
+++ web/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import JsonResponse
+from . import models
 
-# Create your views here.
+class IndexPage:
+    template_name="web/index.html"
+    context = {
+        "title" : "Test title",
+    }
+    
+    @staticmethod
+    def as_view(request):
+        owner = get_object_or_404(models.Owner, pk=1)
+        projects = models.Project.objects.all()
+        
+        IndexPage.context["owner"] = owner
+        IndexPage.context["projects"] = projects
+        IndexPage.context["projects_range"] = range(len(projects))
+        
+        return render(request,
+                      IndexPage.template_name, 
+                      IndexPage.context)
